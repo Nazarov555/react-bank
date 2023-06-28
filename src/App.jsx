dbde8ef0700371abab0be2dcac680ede7f9cd5c8
Nazarov555 <nzrvkeskil@mail.ru>
2023-06-29 02:29:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './style';
+
+const App = () => {
+  return (
+    <div className="
+      bg-primary 
+      w-full overflow-hidden"
+    >
+      <h1 className="text-white">Hello</h1>
+    </div> 
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,206 @@
+import { people01, people02, people03,
+     facebook, instagram, linkedin, 
+     twitter, airbnb, binance, 
+     coinbase, dropbox, send, 
+     shield, star, telegram } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Дом",
+  },
+  {
+    id: "features",
+    title: "Особенности",
+  },
+  {
+    id: "product",
+    title: "Продукт",
+  },
+  {
+    id: "clients",
+    title: "Клиенты",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Награды",
+    content:
+      "Лучшие кредитные карты предлагают заманчивую комбинацию акций и призов.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Безопасно",
+    content:
+      "Мы предпринимаем активные шаги, чтобы обеспечить безопасность вашей информации и транзакций.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Перевод Баланса",
+    content:
+      "Кредитная карта с переводом баланса может сэкономить вам много денег на изменении процентов.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Деньги- это только инструмент. Он доставит вас, куда вы пожелаете, но не заменит вас как водителя.",
+    name: "Лериан Дженсен",
+    title: "Основатель & Лидер",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Деньги делают вашу жизнь проще, если вам повезло, что они у вас есть, вам повезло!",
+    name: "Стив Марк",
+    title: "Основатель & Лидер",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Обычно по-настоящему богатые люди, занимающиеся бизнесом, финансами и международной торговлей.",
+    name: "Кенн Галлагер",
+    title: "Основатель & Лидер",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Активных Пользователей",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Доверенных компаний",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Транзакций",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Полезные ссылки",
+    links: [
+      {
+        name: "Содержание",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "Как это работает",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Создать",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Изучить",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Условия & Услуги",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Сообщество",
+    links: [
+      {
+        name: "Центр Помощи",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Партнёры",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Рекомендации",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Блог",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Новости",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Партнёр",
+    links: [
+      {
+        name: "Наш Партнёр",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Стать Партнёром",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: telegram,
+    link: "https://t.me/Learning_C_sharp_with_me",
+  }, 
+  {
+    id: "social-media-2",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-5",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
